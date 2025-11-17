10d2b05ef205b9bd46b5576a2f9afce0f3a87918
Kyoka Ono <kyoniconico88@gmail.com>
2025-11-17 15:11:22 -0500

    update

--- tests/testthat/test-linearregression.R
+++ tests/testthat/test-linearregression.R
@@ -1,3 +1,10 @@
+library(datasets)
+data(iris)
+data <- iris[iris$Species == "setosa",]
+Y <- data$Sepal.Length
+X <- cbind(data$Petal.Length, data$Petal.Width)
 test_that("linear regression works", {
-  expect_equal(2 * 2, 4)
+  expect_equal(as.vector(linearregression(X,Y)$beta_hat), as.vector(lm(Y~X)$coefficient))
+  expect_equal(as.vector(linearregression(X,Y)$yhat), as.vector(lm(Y~X)$fitted.values))
+  expect_equal(as.vector(linearregression(X,Y)$res), as.vector(lm(Y~X)$residuals))
 })
