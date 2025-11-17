75eb1d87ccbe5c7d9a7eceb05e543a9635208e34
Kyoka Ono <kyoniconico88@gmail.com>
2025-11-17 14:43:20 -0500

    update package

---
+++ R/LinearRegressionFunction.R
@@ -0,0 +1,21 @@
+#' Linear Regression Function
+#'
+#' Conducts linear regression model and computes coefficient estimates, fitted values, and residuals.
+#'
+#' @param X input value
+#'
+#' @param Y input value
+#'
+#' @return the coefficient estimates, fitted values, and residuals
+#'
+#' @examples linearregression(X,Y)
+#'
+#' @export
+#'
+linearregression <- function(X,Y){
+  X <- cbind(1,X)
+  beta_estimates <- solve(t(X) %*% X) %*% t(X) %*% Y
+  fitted_values <- X %*% beta_estimates
+  residuals <- Y - fitted_values
+  return(list(beta_hat = beta_estimates, yhat = fitted_values, res = residuals))
+}
